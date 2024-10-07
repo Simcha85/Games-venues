660b705989b21062fcbf68f92d9bfe8b08d9d93d
Simcha85 <169007313+Simcha85@users.noreply.github.com>
2024-10-08 11:17:45 +1300

    Update Games.py

--- Games.py
+++ Games.py
@@ -1,17 +1,23 @@
 import numpy as np
 import pandas as pd
-import folium
 import streamlit as st
-import matplotlib as plt
 import plotly.express as px
 import warnings
 
-sa_map=folium.Map(location=[-30.5595,22.9375], zoom_start=5)
 
 st.title('KAIZER CHIEFS VENUES USED IN SOUTH AFRICA')
+st.logo('KC logo black back.jpg')
+st.image('fnb.jpg')
 
 df=pd.read_excel('STADIUM info.xlsx')
 df
+
+def get_kc_map():
+    HtmlFile=open('games_map.html','r',encoding='utf-8')
+    games_map=HtmlFile.read()
+    return games_map
+
+games_map=get_kc_map()
 
 games_df=df[['STADIUM','Result','Latitude','Longitude']]
 
@@ -24,28 +30,6 @@ def assign_marker_color(Result):
         return 'red'
     else:
         return 'blue'
-games_df.loc[:,'marker_color']=games_df.loc[:,'Result'].apply(assign_marker_color)
-games_df.head()
-
-
-
-label=games_df['STADIUM']
-lats=games_df['Latitude']
-long=games_df['Longitude']
-
-for lat,lng,label in zip(lats,long,label):
-    folium.Marker([lat,lng],popup=label).add_to(sa_map)
-                                                
-sa_map.save('sa_maptest.html')
-
-import streamlit.components.v1 as components
-
-def get_kc_map():
-    HtmlFile=open('sa_maptest.html','r',encoding='utf-8')
-    games_map=HtmlFile.read()
-    return games_map
-
-games_map=get_kc_map()
 
 def count_result(Result):
     if Result =='Win':
@@ -54,7 +38,8 @@ def count_result(Result):
         return 1
     else:
         return 1
-games_df.loc[:,'Event_count']=games_df.loc[:,"Result"].apply(count_result)
+games_df['Event_count']=games_df.loc[:,"Result"].apply(count_result)
+
 
 line_data=games_df['STADIUM'].value_counts()
 fig=px.line(line_data, markers=True, title='Number of Games played in each Stadium')
@@ -73,5 +58,6 @@ fig2.update_layout(
               )
 st.write(fig2)
 
+import streamlit.components.v1 as components
 with st.container():
     components.html(games_map,width =2000, height=1000)
