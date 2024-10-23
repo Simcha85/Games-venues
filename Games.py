bc409ac1101180f1c5af2980630fbbd18f023a35
Simcha85 <169007313+Simcha85@users.noreply.github.com>
2024-10-23 17:42:07 +1300

    Update Games.py 23.10.2024

--- Games.py
+++ Games.py
@@ -4,13 +4,33 @@ import streamlit as st
 import plotly.express as px
 import warnings
 
+def stats(df):
+    st.header('Data Statistics')
+    st.write(df['Home'].value_counts())
+
+def headers(df):
+    st.header('Data Header')
+    st.write(df.tail())
+
+def games_played(games_df):
+    st.header('Games Played at Each Venue')
+    st.plotly_chart(fig)
+
+def results(games_df):
+    st.header('Results At Venues')
+    st.plotly_chart(fig2)
 
 st.title('KAIZER CHIEFS VENUES USED IN SOUTH AFRICA')
 st.logo('KC logo black back.jpg')
 st.image('fnb.jpg')
 
+st.sidebar.title('Navigation')
+
+options=st.sidebar.radio('Pages', options=['Data Statistics','Data Header', 'Venue Count','Result Tally'])
+
+
 df=pd.read_excel('STADIUM info.xlsx')
-df
+
 
 def get_kc_map():
     HtmlFile=open('games_map.html','r',encoding='utf-8')
@@ -30,6 +50,7 @@ def assign_marker_color(Result):
         return 'red'
     else:
         return 'blue'
+games_df.loc[:,'Outcome']=games_df.loc[:,'Result'].apply(assign_marker_color)
 
 def count_result(Result):
     if Result =='Win':
@@ -48,7 +69,7 @@ fig.update_layout(
     width=800,
     height=600
 )
-st.write(fig)
+
 
 fig2=px.bar(games_df,x='STADIUM',y='Event_count',color='Result', title='Results for Each Venue')
 fig2.update_layout(
@@ -56,7 +77,23 @@ fig2.update_layout(
     width=800,
     height=600
               )
-st.write(fig2)
+
+
+fig3=px.bar(df, x='Home',y='Away',color='Result')
+fig3.update_layout(
+    showlegend=True,
+    width=800,
+    height=600
+)
+
+if options=='Data Statistics':
+    stats(df)
+if options=='Venue Count':
+    games_played(games_df)
+if options=='Result Tally':
+    results(games_df)
+elif options=='Data Header':
+    headers(df)
 
 import streamlit.components.v1 as components
 with st.container():
